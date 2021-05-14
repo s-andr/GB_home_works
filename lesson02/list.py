db132e0715cf659b8d299bfbac8714b38205ea8a
s-andr <49696318+s-andr@users.noreply.github.com>
2021-05-14 13:21:24 +0300

    Add files via upload

---
+++ lesson02/list.py
@@ -0,0 +1,19 @@
+numbers = [33, 15, 52, 76, 47, 25]
+print(numbers)
+
+
+numbers.append(4)
+print(numbers)
+
+numbers.remove(15)
+print(numbers)
+
+
+numbers.extend([201,102,3])
+print(numbers)
+
+numbers.sort()
+print(numbers)
+
+numbers.reverse()
+print(numbers)
---
+++ lesson02/string.py
@@ -0,0 +1,13 @@
+passport = "1234020304"
+passport_serial = passport[0:4]
+passport_number = passport[4:]
+
+print(passport_serial, passport_number)
+
+
+s_code = "934A5673"
+country_code = s_code[3]
+print(country_code)
+
+revers_string = "programming"
+print((revers_string[::-1]))
